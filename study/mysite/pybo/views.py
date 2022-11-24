413dec749e2c6a00893af4616647a54fdea72b90
hugo <hgk9872@gmail.com>
2022-11-24 22:25:33 +0900

    20221124 django study

--- study/mysite/pybo/views.py
+++ study/mysite/pybo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from django.utils import timezone
+from .forms import QuestionForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -10,3 +12,21 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question} # key, value
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False) # 임시 저장하여 question 객체 리턴
+            question.create_date = timezone.now() # 'subject', 'content'는 form에서 등록 (Meta를 통해)
+            question.save() # 실제로 저장 (객체 생성)
+            return redirect('pybo:index') # 데이터(질문 객체) 저장 후, 홈으로 돌아가기
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
