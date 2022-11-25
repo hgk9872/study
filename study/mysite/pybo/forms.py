91d93ebd17eb2c4f40980ef3064a4a6041a87c33
hugo <hgk9872@gmail.com>
2022-11-25 21:03:44 +0900

    20221125

--- study/mysite/pybo/forms.py
+++ study/mysite/pybo/forms.py
@@ -1,15 +1,19 @@
 from django import forms
-from .models import Question
+from .models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question      # 사용할 모델
         fields = ['subject', 'content'] # QuestionForm에서 사용할 Question모델의 속성
-        widgets = {
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-        }
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content'] # field는 사용할 속성
+        labels = {           # labels은 그 속성에 대한 라벨(제목)
+            'content' : '답변내용',
         }
