c701879956ea247e597aa04b4b25fa8718be092a
hugo <hgk9872@gmail.com>
2022-12-04 01:48:28 +0900

    Q44 sol

---
+++ coding_test/ch18/44.py
@@ -0,0 +1,54 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+    
+# 행성의 개수 n
+n = int(input())
+
+# 부모리스트 초기화
+parent = [x for x in range(n + 1)]
+
+x = []
+y = []
+z = []
+
+for i in range(1, n + 1):
+    a, b, c = map(int, input().split())
+    x.append((a, i))
+    y.append((b, i))
+    z.append((c, i))
+
+x.sort()
+y.sort()
+z.sort()
+
+edges = []
+
+for i in range(n - 1):
+    edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
+    edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
+    edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+
+edges.sort()
+
+result = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
